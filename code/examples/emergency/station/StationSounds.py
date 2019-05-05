d6f27d53a50d4d367c2fc6e2909089e1a47ee448
varvarigos <45434379+varvarigos@users.noreply.github.com>
2019-05-05 17:11:08 +0300

    Update StationSounds.py

--- code/examples/emergency/station/StationSounds.py
+++ code/examples/emergency/station/StationSounds.py
@@ -9,6 +9,10 @@ import pygame
 
 from PyQt5.QtCore import QThread
 
+"""
+   H κλάση παρέχει το μηχανισμό για την αναπαραγωγή ενός αρχείου ήχου. 
+"""
+
 class StationSounds(QThread):
 
 	def __init__(self):
@@ -22,6 +26,10 @@ class StationSounds(QThread):
 		self.wait()
 
 
+	"""
+	   Η μεθοδος μας επιτρέπει να ορίσουμε το αρχείο ήχου που θα αναπαραχθεί μέσω της 
+	   μεθόδου run()
+	"""
 	def on_play_sound(self, file_name):
 		
 		self.play_sound = False
@@ -29,20 +37,31 @@ class StationSounds(QThread):
 		self.play_sound = True
 		print(self.sound_file_name)
 
+	"""
+	   Είναι η βασική μέθοδος που τρέχει διαρκώς και ανάλογα με την τιμή που έχει η μεταβλητή play_sound 
+	   αρχίζει την αναπαραγωγή του αρχείου ήχου που περιέχει η μεταβλητή sound_file_name
+	   Επιλέξαμε αυτό την οργανωση γιατι θέλουμε το μηνυμα εκκενωσης να ακουγεται συνεχως σε περίπτωση 
+	   κάποιας έκτακτης ανάγκης.
+	"""
 	def run(self):
 
+		# Οι επόμενες δύο εντολές αρχικοποιούν την βιβλιοθήκη pygame μέσω της οποίας 
+		# γίνεται η αναπαραγωγή των αρχείων ήχου
 		pygame.mixer.pre_init(frequency = 16000, channels = 1) 
 
 		pygame.init()
 
 		while True:
 
-			if self.play_sound :
+			# Αν η μεταβλητη είναι True τότε αρχίζει την αναπαραγωγή του αρχείου
+			if self.play_sound :   
 
+				#οριζουμε τις παραμετρους, το αρχείο και αρχίζουμε την αναπαραγωγή
 				pygame.mixer.init(frequency = 16000, channels = 1)
 				pygame.mixer.music.load(self.sound_file_name)
 				pygame.mixer.music.play()
 
+				#περιμενουμε μεχρι να τελειωσει η τρεχουσα αναπαραγωγη
 				while pygame.mixer.music.get_busy() == True:
 					continue
  
