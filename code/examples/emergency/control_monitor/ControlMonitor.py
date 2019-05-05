539e2d2744b5a10929db94ff1740ae32a434749c
varvarigos <45434379+varvarigos@users.noreply.github.com>
2019-05-05 21:53:16 +0300

    Update ControlMonitor.py

--- code/examples/emergency/control_monitor/ControlMonitor.py
+++ code/examples/emergency/control_monitor/ControlMonitor.py
@@ -25,21 +25,33 @@ import openmic.controller.controller_instance as controller_instance
 
 COLORS = [Qt.white, Qt.black, Qt.lightGray, Qt.green, Qt.yellow]
 
+"""
+  Η βασική κλάση κληρονομεί απο την κλάση QMainWindow της βιβλιοθήκης PyQt5 καθως περιλαμβάνει τόσο την 
+  υλοποίηση της λογικής της εφαρμογής όσο και της γραφικής διεπαφής.
+  
+  Ο σχεδιασμός της γραφικής διεπαφής (ui/MainWindow.ui) έγινε με την βοήθεια του διαθέσιμου εργαλείου Qt Designer 
+  ενώ για την δημιουργία του σχετικού κώδικα (MainWindow.py) χρησιμοποιήσαμε την εντολή pyuic5.
+  
+"""
 class ControlMonitor(QMainWindow, MainWindow.Ui_MainWindow):
 
+	# Ορίζουμε 2 βοηθητικά signals χρησιμοποιώντας την βιβλιοθήκη PyQt5, αφορούν αποκλειστικά
+	# λειτουργίες που σχετίζονται με την λειτουργία της γραφικής διεπαφής
 	refresh_signal = pyqtSignal()
 	agent_disconnects_signal = pyqtSignal(events.EventAgentsDisconnected)
 
 	def __init__(self):
 	
 		super(ControlMonitor, self).__init__()
-
+		
 		self.instance = None
 		self.stations_status = []
 		self.stations_agent_id = {}
 
 		self.setupUi(self)
 
+		# Διαβάζουμε το αρχείο map.json που περιέχει την απεικόνιση του χώρου
+		# που εμφανίζεται στην γραφική διεπαφή του κέντρου ελέγχου
 		with open("map.json") as f:
 			data = json.load(f)
 			self.map_data = data["map"]
@@ -58,40 +70,70 @@ class ControlMonitor(QMainWindow, MainWindow.Ui_MainWindow):
 
 	def setup_openmic_controller(self):
 		
+		# Καθορίζουμε την παράμετρο για την διεπαφή επικοινωνίας με βάση το ZeroMQ 
 		zeromq_params = cons.ZeroMQParameters("127.0.0.1")
+		
+		# Δημιουργία αντικειμένου της βοηθητικής κλάσης ControllerContext με βάση τις παραμέτρους για την διεπαφή επικοινωνίας
 		context = controller_context.ControllerContext(zeromq_params)
 
+		"""
+		   Αν θέλουμε να χρησιμοποιήσουμε την διεπαφή επικοινωνίας με βάση το RabbitMQ θα πρέπει να σχολιαστούν οι
+		   δυο παραπάνω γραμμές και να βγούν απο τα σχόλια οι επόμενες δύο, αφού πρώτα ορίσουμε τις κατάλληλες
+		   παραμέτρους στην RabbitMQParameters 
+		"""
 		#rabbitmq_params = cons.RabbitMQParameters("","", "", virtual_host = "")
 		#context = controller_context.ControllerContext(rabbitmq_params, constants.DRIVER_RABBITMQ)
 		
+		# Δημιουργούμε το αντικείμενο της βασικής κλάσης ControllerInstance που παρέχει τις λειτουργίες της
+		# βιβλιοθήκης OpenMIC στην εφαρμογή μας
 		self.instance = controller_instance.ControllerInstance(context)
 		
+		# Μας ενδιαφέρει να παρακολουθούμε 4 απο τα μηνύματα που σχετίζονται με την επικοινωνία μεταξύ της 
+		# εφαρμογής ελέγχου και διαχείρισης και των ειδικών εφαρμογών, έτσι κάνουμε εγγραφή στα αντίστοιχα events 
+		# ορίζουμε τις μεθόδους που θέλουμε να τα χειρίζονται
 		context.connect([events.EventAgentConnected], self.agent_connect)
 		context.connect([events.EventAgentsDisconnected], self.agent_disconnect)
 		context.connect([events.EventAgentHeartBeat], self.agent_heartbeat)
 		context.connect([events.EventAgentGeneralMessage], self.agent_message)
 
+		# Συσχετίζουμε τα 2 βοηθητικά signals που ορίσαμε στην αρχή της κλάσης με τις μεθόδους που τα χειρίζονται
 		self.refresh_signal.connect(self.refresh_map)
 		self.agent_disconnects_signal.connect(self.disconnect_alert)
 
+	"""
+	   Η παρακάτω μέθοδος χρησιμοποιεί τον μηχανισμό χειρισμού γεγονότων σε στοιχεία της γραφικής
+	   διεπαφής της PyQt5 για να ορίσει τις μεθόδους που εκτελούν τις αντίστοιχες ενέργειες.
+	   Πρόκειται για ενέργειες που σχετίζονται με την γραφική διεπαφή και την βιβλιοθήκη PyQt5 και οχι
+	   με την βιβλιοθήκη OpenMIC.
+	"""		
 	def setup_actions(self):
 
+		# Menu action
 		self.actionAbout.triggered.connect(self.credentials)
 
+		# Actions για τα buttons 
 		self.ledsButton.clicked.connect(self.toggle_led_blinking_rate)
 		self.testSoundButton.clicked.connect(self.sound_testing)
 		
-
+	"""
+	   Προωθεί σε όλους τους διαθέσιμους σταθμούς την εντολή που αντιστοιχεί στον έλεγχο ήχου
+	"""
 	def sound_testing(self):
 
 		self.instance.forward_request({"cmd": "sound_testing"})
 
-
+	"""
+	   Προωθεί σε όλους τους διαθέσιμους σταθμούς την εντολή που αντιστοιχεί στην αλλαγή του
+	   ρυθμού που λειτουργούν τα LEDs
+	"""
 	def toggle_led_blinking_rate(self):
 
 		self.instance.forward_request({"cmd": "led_blinkig"})
 
-
+		
+	"""
+	   Ανανεώνει την απεικόνιση του χώρου στη γραφική διεπαφή
+	"""
 	def refresh_map(self):
 
 		stations = 0
@@ -125,7 +167,9 @@ class ControlMonitor(QMainWindow, MainWindow.Ui_MainWindow):
 
 				self.gridLayout.addWidget(lbl, row, column)
 			
-
+	"""
+	  Εμφανίζει το παράθυρο με τις πληροφορίες σχετικά με την εφαρμογή
+	"""
 	def credentials(self):
 
 		message = '<b>Ανδρέας Βαρβαρίγος & Αναστάσης Βαρβαρίγος 2019</b><br><br>'
@@ -141,34 +185,50 @@ class ControlMonitor(QMainWindow, MainWindow.Ui_MainWindow):
 		dlg.show()
 
 
+	"""
+	  Ενημερώνει το χρήστη όταν εντοπιστεί κάποια απώλεια επικοινωνίας με κάποιο σταθμό
+	"""
 	def disconnect_alert(self, evt):	
 		self.critical_message("Απώλεια επικοινωνίας με τους ακόλουθους σταθμούς παρατήρησης: %s"%(" , ".join(evt.agents_ids)))
 
-
+	"""
+	   Χειρίζεται τα events απο την βιβλιοθήκη OpenMIC σχετικά με την αναγνώριση σύνδεσης απο κάποιο σταθμό
+	"""
 	def agent_connect(self, evt):
 
+		# Ενημερώνουμε κατάλληλα τις βοηθητικές μεταβλητές
 		if evt.agent_id not in self.stations_agent_id :
 			self.stations_agent_id[evt.agent_id] = { "station_index": -1 }
 		else:
 			station_index = self.stations_agent_id[evt.agent_id]["station_index"]
 			self.stations_status[station_index] = 1
 
+		# Στέλνουμε ένα σήμα στην γραφική διεπαφή ώστε να ανανεωθεί η απεικόνιση του χώρου 
 		self.refresh_signal.emit()
 
-
+	
+	"""
+	   Χειρίζεται τα events απο την βιβλιοθήκη OpenMIC σχετικά με την αναγνώριση σύνδεσης απο κάποιο σταθμό
+	"""
 	def agent_disconnect(self, evt):
-
+		
+		# Ενημερώνουμε κατάλληλα τις βοηθητικές μεταβλητές
 		for agent_id in evt.agents_ids:
 			self.stations_status[self.stations_agent_id[agent_id]["station_index"]] = 0
 
+		# Στέλνουμε ένα σήμα στην γραφική διεπαφή ώστε να ανανεωθεί η απεικόνιση του χώρου 
 		self.agent_disconnects_signal.emit(evt)
 
-
+	"""
+	  Χειρίζεται events απο την βιβλιοθήκη OpenMIC σχετικά με την λήψη γενικών μηνυμάτων από κάποιο σταθμό
+	"""
 	def agent_message(self, evt):
-
+		
+		# Ενημερώνουμε κατάλληλα τις βοηθητικές μεταβλητές
 		self.stations_agent_id[evt.agent_id]["station_index"] = evt.message["station_index"]
 		self.stations_status[evt.message["station_index"]] = evt.message["status"]
-
+		
+		# Στέλνουμε ένα σήμα στην γραφική διεπαφή ώστε να ανανεωθεί η απεικόνιση του χώρου 
 		self.refresh_signal.emit()
 
 
@@ -183,13 +243,20 @@ class ControlMonitor(QMainWindow, MainWindow.Ui_MainWindow):
 		dlg.setIcon(QMessageBox.Critical)
 		dlg.show()
 
-
+		
+"""
+ Το βασικό σημείο απο το οποίο ξεκινά η εκτέλεση του προγράμματος
+"""
 if __name__ == '__main__':
 
 	logging.basicConfig(filename = 'control_monitor.log', level = logging.INFO)
 
 	app = QApplication([])	
 	app.setApplicationName("Emergency - Control Monitor")
+	
+	# Δημιουργούμε ένα αντικείμενο της κλάσης ControlMonitor που έχουμε υλοποιήσει και περιλαμβάνει
+	# την γραφική διεπαφή (βασίζεται στη βιβλιοθήκη PyQt5) και τη λογική της εφαρμογής ελέγχου και διαχείρισης (βασίζεται 
+	# στην βιβλιοθήκη OpenMIC
 	window = ControlMonitor()
 
 	app.exec_()
