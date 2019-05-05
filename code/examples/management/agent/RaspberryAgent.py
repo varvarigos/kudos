7c49cdddc4000573ee6700aaa6c16daad4637c16
varvarigos <45434379+varvarigos@users.noreply.github.com>
2019-05-05 17:46:24 +0300

    Update RaspberryAgent.py

--- code/examples/management/agent/RaspberryAgent.py
+++ code/examples/management/agent/RaspberryAgent.py
@@ -23,10 +23,20 @@ class RaspberryAgent:
 
 	def __init__(self) :
 		
+		# Μεταβλητή που 
 		self.panding_requests = {}
-
+		
+		"""
+		  Οι επόμενες δυο γραμμές ορίζουνε ++++
+		  +++
+		"""
 		zeromq_params = cons.ZeroMQParameters("127.0.0.1")
 		self.context = agent_context.AgentContext(zeromq_params)
+		
+		"""
+		   Οι επόμενες
+		   Αν 
+		"""
 		
 		#rabbitmq_params = cons.RabbitMQParameters("","", "", virtual_host = "")
 		#self.context = agent_context.AgentContext(rabbitmq_params, constants.DRIVER_RABBITMQ)
@@ -34,15 +44,26 @@ class RaspberryAgent:
 
 	def start(self):
 
+		# Δημιουργούμε το αντικείμενο της βασικής κλάσης AgentInstance που παρέχει τις λειτουργίες της
+		# βιβλιοθήκης OpenMIC στην εφαρμογή μας
 		self.agent_instance = agent_instance.AgentInstance(self.context) 
 
+		# Μας ενδιαφέρει μόνο να παρακολουθούμε για μηνύματα με εντολές απο την εφαρμογή ελέγχου και διαχείρισης,
+		# έτσι κάναμε εγγραφή μονο για αυτό το event και ορίσαμε οτι τα σχετικά events θέλουμε
+		# να τα χειρίζεται η μέθοδος handle_controller_request
 		self.context.connect([events.EventControllerRequest], self.handle_controller_request)
 
 
+	# Χειρίζεται τα μηνύματα με τις εντολές, εκτελεί τις κατάλληλες ενέργειες και επιστρέφει τα αποτελέσματα
 	def handle_controller_request(self, evt):
 
 		request = evt.request
 
+		# Ελέγχουμε το πεδιο "cmd" αφού αυτό καθορίζει το είδος της εντολής, άρα και τις ενέργειες 
+		# που πρέπει να εκτελεστούν, σε κάθε περίπτωση χρησιμοποιούμε την κατάλληλη μέθοδο 
+		# απο το αρχείο agent_operations.py
+		
+		# Εντολή καταγραφής
 		if request["cmd"] == "inventory" :
 
 			if len(request["params"]) == 0 :
@@ -52,25 +73,36 @@ class RaspberryAgent:
 
 			response = agent_operations.inventory(params)
 		
+		# Εκτέλεση εντολής συστήματος
 		if request["cmd"] == "shell_command" :
 			response = agent_operations.execute_shell_command(request["params"])
 
+		# Εκτέλεση γενικής εντολής
 		if request["cmd"] == "simple_command" :
 			response = agent_operations.execute_simple_command(request["params"])
 
+		# Εγκατάσταση πακέτου λογισμικού
 		if request["cmd"] == "install_package" :
 			response = agent_operations.install_package(request["params"])
 
+		# Απεγκατάσταση πακέτου λογισμικού
 		if request["cmd"] == "uninstall_package" :
 			response = agent_operations.uninstall_package(request["params"])
 
+		# Στο τελικό βήμα επιστρέφουμε μέσω της κατάλληλης μεθόδου της βιβλιοθήκης OpenMIC
+		# το αποτέλεσμα της εντολής προς την εφαρμογή ελέγχου και διαχείρισης
 		self.agent_instance.send_response(evt.request_id, response)
 		
-
+"""
+ Το βασικό σημείο απο το οποίο ξεκινά η εκτέλεση του προγράμματος
+"""
 if __name__ == "__main__":
 
 	logging.basicConfig(filename = 'agent.log', level = logging.INFO)
 
+	# Δημιουργούμε ένα αντικείμενο της κλάσης RaspberryAgent που έχουμε υλοποιήσει και περιλαμβάνει
+	# την λογική της ειδικής εφαρμογής που βασίζεται στην βιβλιοθήκη OpenMIC
 	agent = RaspberryAgent()
 
+	# Ξεκινά η εκτέλεση της ειδικής εφαρμογής 
 	agent.start()
